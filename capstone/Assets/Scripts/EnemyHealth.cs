408f64698f5e87fabc064076215ad70b305bb46f
LyndenJT17 <64235625+LyndenJT17@users.noreply.github.com>
2023-04-10 19:31:23 -0500

    coin

--- capstone/Assets/Scripts/EnemyHealth.cs
+++ capstone/Assets/Scripts/EnemyHealth.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class EnemyHealth : MonoBehaviour
 {
+    
     public int health;
     public int currentHealth;
+
+    public GameObject item;
+    public Transform transform;
+
+    public string spawnPointTag = "sometag";
+    public bool alwaysSpawn = true;
+    public List<GameObject> prefabsToSpawn;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,11 +26,24 @@ public class EnemyHealth : MonoBehaviour
         if (currentHealth <= 0)
         {
             Destroy(gameObject);
+            DropItem();
         }
     }
+    
 
     public void damageEnemy(int damage)
     {
         currentHealth -= damage;
+    }
+    void DropItem()
+    {
+        Vector2 pos = transform.position;
+        //GameObject loot = Instantiate(item, pos, Quaternion.identity);
+
+        int randomPrefab = Random.Range(0, prefabsToSpawn.Count);
+        GameObject pts = Instantiate(prefabsToSpawn[randomPrefab]);
+        pts.transform.position = pos;
+
+
     }
 }
